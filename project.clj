c998916c457b60c6636f0b6ee4cd757c7c3a437c
James Reeves <jreeves@weavejester.com>
2014-02-12 15:04:58 +0000

    Added url and license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject snowball-stemmer "0.1.0"
   :description "Provides simple wrapper for tartarus.org stemmer"
+  :url "https://github.com/weavejester/snowball-stemmer"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
